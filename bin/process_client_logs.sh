f4ecc93c0da8b177a3b89c08f8bdfaad29e2c0ba
hamer073 <hamer073@morris.umn.edu>
2018-09-06 11:49:32 -0500

    Fixed process_client_logs.sh so that the output file ends up in the correct directory Still need to make the outputed lines fully correct

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,3 +1,4 @@
 Directory=$1
-cd $1/var/log
-cat secure* | awk '/Failed password/ {if ($9 == "Invalid") {print $1,$2,$3,$11,$13} else {print $1,$2,$3,$9,$11}' | sed -r 's/:[[:digit:]]+//' > failed_login_data.txt
+cd $Directory/var/log
+cat secure* | awk '/Failed password/ {if ($9 == "invalid") {print $1,$2,$3,$11,$13} else {print $1,$2,$3,$9,$11}} ' | sed -r 's/:[[:digit:]]+//' > failed_login_data.txt
+mv failed_login_data.txt $Directory
