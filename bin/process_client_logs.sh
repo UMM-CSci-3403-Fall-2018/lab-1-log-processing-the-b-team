d67d3ff7aedd22863a6d93958f626c93e937e939
M1NN <score@morris.umn.edu>
2018-09-11 10:14:41 -0500

    Fixed issue where output for process_client_logs would be put into wrong directory with the wrong file name

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,4 +1,4 @@
 Directory=$1
 cd $Directory/var/log
 cat secure* | awk '/Failed password/ {if ($9 == "invalid") {print $1,$2,$3,$11,$13} else {print $1,$2,$3,$9,$11}} ' | sed -r 's/:[[:digit:]]+:[[:digit:]]+//' > failed_login_data.txt
-mv failed_login_data.txt $Directory
+mv failed_login_data.txt ../../
