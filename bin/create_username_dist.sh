9e484cafd95bc23b8fc15430dc445df922c70b71
M1NN <score@morris.umn.edu>
2018-09-11 11:18:20 -0500

    Fixed awk arguements so now they print out the correct information in the correct format create_username_dist now passes all the test wrap_contents is now excecutable

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,2 +1,5 @@
 Directory=$1
-cat $1/*/failed_login_data.txt | awk '{print $4}'
+cat $1/*/failed_login_data.txt | awk '{print $4}' | sort | uniq -c | awk '{print "data.addRow([\x27"$2"\x27, "$1"]);"}' > username.txt
+bin/wrap_contents.sh username.txt html_components/username_dist username_dist.html
+rm username.txt
+mv username_dist.html $1
