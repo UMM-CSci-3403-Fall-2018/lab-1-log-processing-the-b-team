c43b89266c018a661675d55ed110c72cf32487e3
hamer073 <hamer073@morris.umn.edu>
2018-09-12 18:42:48 -0500

    Write the rest of process logs -Remove -v from tar because who needs all that output -Fix $Directory to get the correct name (finally) -Add calls to all the other shell files Remove cd from process_client_logs so that we stay in one directory

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,4 +1,3 @@
 Directory=$1
-cd $Directory/var/log
-cat secure* | awk '/Failed password/ {if ($9 == "invalid") {print $1,$2,$3,$11,$13} else {print $1,$2,$3,$9,$11}} ' | sed -r 's/:[[:digit:]]+:[[:digit:]]+//' > failed_login_data.txt
-mv failed_login_data.txt ../../
+cat log_files/$Directory/var/log/secure* | awk '/Failed password/ {if ($9 == "invalid") {print $1,$2,$3,$11,$13} else {print $1,$2,$3,$9,$11}} ' | sed -r 's/:[[:digit:]]+:[[:digit:]]+//' > failed_login_data.txt
+mv failed_login_data.txt log_files/$Directory
--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,8 +1,16 @@
 tempDirectory=$(mktemp -d)
+startingDirectory=$(pwd)
 for var in "$@"
 do
-	Directory="$(var%_.*)"
-	mkdir $Directory
-	tar -C $Directory -xzvf log_files/$var
-	mv $Directory $tempDirectory
+	Directory="$(basename $var '_secure.tgz')"
+	mkdir log_files/$Directory
+	tar -C log_files/$Directory -xzf $var
+	bin/process_client_logs.sh $Directory
+	mv log_files/$Directory $tempDirectory
 done
+bin/create_username_dist.sh $tempDirectory
+bin/create_hours_dist.sh $tempDirectory
+bin/create_country_dist.sh $tempDirectory
+bin/assemble_report.sh $tempDirectory
+mv $tempDirectory/failed_login_summary.html $startingDirectory/failed_login_summary.html
+rm -rf $tempDirectory
